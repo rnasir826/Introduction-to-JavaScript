6bc4b3fe223800a47f2a149cfb52e33de5db864e
Ramsha <ramshanasir.rn@gmail.com>
2020-09-08 18:21:21 -0500

    done

--- index.js
+++ index.js
@@ -2,10 +2,22 @@
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
 
-
+const votingAge = 19;
+if (votingAge >= 18) {
+    console.log(true);
+} else {
+    console.log(false)
+}
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
+
+let game = win {
+    if (game !== win);
+    console.log(looser);
+} else(game === win) {
+    console.log(winner);
+}
 
 
 
@@ -13,22 +25,32 @@
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
-
+let str = "1999";
+parseInt(str);
 
 
 
 //Task d: Write a function to multiply a*b 
 
-
-
+function multiply(a, b) {
+    return a * b
+}
+let math = multiply(10, 36)
+console.log(math)
 
 
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
 
-
-
+function ageInDogYears(age) {
+    let dogAge = 7 * age;
+    console.log("Pupper is " + dogAge + " years old");
+}
+ageInDogYears(1);
+ageInDogYears(17);
+ageInDogYears(24);
+ageInDogYears(.3);
 
 
 /************************************************************** Task 3 **************************************************************/
@@ -38,81 +60,153 @@
 //feeding requirements
 // adult dogs at least 1 year 
 // up to 5 lbs - 5% of their body weight
-// 6 - 10 lbs - 4% of their body weight 
-// 11 - 15 lbs - 3% of their body weight 
-// > 15lbs - 2% of their body weight 
-
-// Puppies less than 1 year
-// 2 - 4 months 10% of their body weight
-// 4 - 7 months 5% of their body weight 
-// 7 - 12 months 4% of their body weight
-
-// when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
-  
+function dogFeeder(weight, age) {
+    if (age => 1) {
+        if (weight <= 5) {
+            return .05 * weight
+        }
 
 
+        // 6 - 10 lbs - 4% of their body weight 
+        else if (6 <= weight <= 10) {
+            return .04 * weight
+        }
 
+        // 11 - 15 lbs - 3% of their body weight 
+        else if (11 <= weight <= 15) {
+            return .03 * weight
+        }
 
-/************************************************************** Task 4 **************************************************************/
-// Rock, Paper, Sissors
-// Your function should take a string (either rock paper or sissors)
-// it should return you won or you lost based on the rules of the game (you may need to look up the rules if you have not played before)
-// use math.random to determine the computers choice 
-// hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
+        // > 15lbs - 2% of their body weight 
+        else {
+            return .02 * weight
+        }
 
-  
-  
-
-/************************************************************** Task 5 **************************************************************/
-//Metric Converter
-//a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
-
-
-
-
-
-//b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
-  
-
-
-
-
-/************************************************************** Task 6 **************************************************************/
-// 99 bottles of soda on the wall
-// create a function called annoyingSong
-// the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-  
-
-
-
-
-/************************************************************** Task 7 **************************************************************/
-//Grade Calculator
-//write a javaScript program that takes a mark out of 100 and returns a corisponding letter grade 
-//90s should be A 
-//80s should be B 
-//70s should be Cs 
-//60s should be D 
-//and anything below 60 should be F
-  
-
-  
-  
-
-/************************************************************** Stretch **************************************************************/
-//Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
-// Hint - you may need to study tomorrow's traning kit on arrays 
-// try looking up the .includes() method
+        // Puppies less than 1 year
+        // 2 - 4 months 10% of their body weight
+        if (age < 1) {
+            if (2 <= weight < 4) {
+                return .1 * weight
+            }
+            // 4 - 7 months 5% of their body weight 
+            else if (4 < weight < 6) {
+                return .05 * weight
+            }
+            // 7 - 12 months 4% of their body weight
+            else {
+                return .04 * weight
+            }
+        }
+        // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
 
 
 
 
 
-/************************************************************** Stretch **************************************************************/
-//Take Rock, Paper, Sissors further
-//update your rock papers sissors code below to take a prompt from a user using the window object
+        /************************************************************** Task 4 **************************************************************/
+        // Rock, Paper, Sissors
+        // Your function should take a string (either rock paper or sissors)
+        // it should return you won or you lost based on the rules of the game (you may need to look up the rules if you have not played before)
+        // use math.random to determine the computers choice 
+        // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
+
+        var userChooses = prompt("Pick rock, paper or scissors?");
+        var compPicks = Math.random();
+        if (compPicks < 0.46) {
+            compPicks = "rock";
+        } else if (compPicks <= 0.82) {
+            compPicks = "paper";
+        } else {
+            compPicks = "scissors";
+        }
+        var compare = function(choice1, choice2) {}
+        if (compPicks === userChooses) {
+            console.log("The result is a tie!");
+        }
+        if (userChooses === "rock") {
+            if (userChooses === "scissors") {
+                console.log("rock wins");
+            } else {
+                console.log("paper wins");
+            }
+        }
+        if (userChooses === "paper") {
+            if (userChooses === "rock") {
+                console.log("paper wins");
+            } else {
+                console.log("scissors wins");
+            }
+        }
+        if (userChooses === "scissors") {
+            if (userChooses === "rock") {
+                console.log("rock wins");
+            } else {
+                console.log("scissors wins");
+            }
+        };
+        compare(userChooses, compPicks);
+
+
+        /************************************************************** Task 5 **************************************************************/
+        //Metric Converter
+        //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
+        var km = parseInt(prompt("Please enter kilometers:"));
+        var miles = km / 1.6;
+        console.log(miles + " Miles");
+
+
+
+
+        //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
+
+        var feet = parseInt(prompt("Please enter feet:"));
+        var cm = feet / 30.48;
+        console.log(cm + " Centimeter ");
+
+
+
+        /************************************************************** Task 6 **************************************************************/
+        // 99 bottles of soda on the wall
+        // create a function called annoyingSong
+        // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
+        let annoyingSong = StartingNum => {
+            for (let i = StartingNum; i > 0; i--) {
+                console.log("startingNum");
+            }
+        }
+        annoyingSong(16);
+
+
+
+        /************************************************************** Task 7 **************************************************************/
+        //Grade Calculator
+        //write a javaScript program that takes a mark out of 100 and returns a corisponding letter grade 
+        //90s should be A 
+        //80s should be B 
+        //70s should be Cs 
+        //60s should be D 
+        //and anything below 60 should be F
+        let marks = 100; {
+            if (marks >= 90);
+            return A;
+        }
+        let B = 80;
+        let C = 70;
+        let D = 60;
+        else let = F;
+
+
+
+
+        /************************************************************** Stretch **************************************************************/
+        //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
+        // Hint - you may need to study tomorrow's traning kit on arrays 
+        // try looking up the .includes() method
 
 
 
 
 
+        /************************************************************** Stretch **************************************************************/
+        //Take Rock, Paper, Sissors further
+        //update your rock papers sissors code below to take a prompt from a user using the window object
